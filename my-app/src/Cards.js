faa7a79bcd988312bac57131fcd7d34e5c80b13a
slahel <74063357+slahel@users.noreply.github.com>
2022-03-14 16:29:02 +0100

    Card set-up

--- my-app/src/Cards.js
+++ my-app/src/Cards.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Point from "./images/Point.png";
 
 export default function Cards() {
   return (
     <div className="cards">
-      <div></div>
+      <img src={Point} />
+      <span className="country"></span>
+      <a className="GoogleMaps"></a>
+      <div className="title"></div>
+      <div className="date"> </div>
+      <div className="description"></div>
     </div>
   );
 }
