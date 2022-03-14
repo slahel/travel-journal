ec1a9467721d0b1f3875ff956a87de9308570e2d
slahel <74063357+slahel@users.noreply.github.com>
2022-03-14 13:51:48 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">My Travel Journal</div>
+    </div>
+  );
 }
 
 export default App;
